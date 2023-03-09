034b20665520537f1d10e04a9d5e746d2e977109
Alex <alexevans2313@gmail.com>
2023-03-08 23:35:16 -0700

    added some quotes

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const quotesBySubject = {
     happiness: [
       "The greatest happiness you can have is knowing that you do not necessarily require happiness. -William Saroyan",
       "Happiness is a warm puppy. -Charles M. Schulz",
-      "Happiness is not something ready made. It comes from your own actions. -Dalai Lama"
+      "Happiness is not something ready made. It comes from your own actions. -Dalai Lama",
+      "The happiness of your life depends upon the quality of your thoughts: therefore, guard accordingly, and take care that you entertain no notions unsuitable to virtue and reasonable nature. -Marcus Aurelius"
     ],
     positivity: [
       "Positive anything is better than negative nothing. -Elbert Hubbard",
@@ -16,13 +17,18 @@ const quotesBySubject = {
     ],
     patience: [
       "Patience is not the ability to wait, but the ability to keep a good attitude while waiting. -Joyce Meyer",
-      "Patience is bitter, but its fruit is sweet. -Aristotle",
-      "Tolerance and patience should not be read as signs of weakness, they are signs of strength. -Dalai Lama"
+      "Patience is bitter, but its fruit is sweet. -Jean-Jaques Rousseau",
+      "Tolerance and patience should not be read as signs of weakness, they are signs of strength. -Dalai Lama",
+      "To lose patience is to lose the battle. -Mahatma Ghandi",
+      "Have patience with all things but first of all with yourself. -Francis de Sales"
     ],
     charity: [
-      "",
+      "It’s not how much we give but how much love we put into giving. -Mother Teresa",
       "The best way to find yourself is to lose yourself in the service of others. -Mahatma Gandhi",
-      "No one has ever become poor by giving. -Anne Frank"
+      "No one has ever become poor by giving. -Anne Frank",
+      "It is a serious thing to live in a society of possible gods and goddesses, to remember that the dullest and most uninteresting person you talk to may one day be a creature which . . . you would be strongly tempted to worship. . . . There are no ordinary people. - CS Lewis",
+      "Men, take care not to make women weep, for God counts their tears. -Thomas S. Monson",
+      "If we are truly disciples of the Lord Jesus Christ, we will reach out with love and understanding to all of our neighbors at all times. -M. Russell Ballard"
     ]
   };
   
